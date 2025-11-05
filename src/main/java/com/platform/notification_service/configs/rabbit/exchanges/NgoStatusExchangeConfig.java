403d6d33750b0437ab16e280fab9a095dffc9b5b
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2025-11-05 10:14:27 -0300

    Add NGO status notification support
    Introduces configuration, DTOs, enums, service interfaces, and implementations for handling NGO status notifications via RabbitMQ and email. Adds templates for pending, rejected, and verified organization status emails, and updates application.yaml with new queue and exchange settings.

---
+++ src/main/java/com/platform/notification_service/configs/rabbit/exchanges/NgoStatusExchangeConfig.java
@@ -0,0 +1,19 @@
+package com.platform.notification_service.configs.rabbit.exchanges;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for the "NGO status" RabbitMQ exchange.
+ */
+@Configuration
+public class NgoStatusExchangeConfig extends ExchangeAbstractConfig {
+    /**
+     * Constructs a NgoStatusExchangeConfig with the specified exchange name.
+     * @param exchangeNameParam the name of the NGO status exchange,
+     *                          injected from application properties
+     */
+    public NgoStatusExchangeConfig(@Value("${queues.ngo-status.exchange}") String exchangeNameParam) {
+        super(exchangeNameParam);
+    }
+}
